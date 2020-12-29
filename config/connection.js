25ef1acb512893aecb17a9e933e811b0b1a516b7
Dennis Francis <dnfrancis22@gmail.com>
2020-12-29 00:55:18 -0500

    updated connection file with jaws db info.

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,23 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "pho12345",
-  database: "burger_db"
-});
+var connection;
+// If the server contains the JAWSDB_URLenvironmental variable, it connects to the JawsDB database.
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  // If the server lacks the variable, it falls back on an explicitly defined local database.
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "pho12345",
+    database: "burger_db",
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
